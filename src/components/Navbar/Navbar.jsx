7c0e2b713799aa7c722f365f1ecb762c954546d2
Pratik_Odpalliwar <pratikodpalliwar777@gmail.com>
2024-01-21 00:47:30 +0530

    Navbar is added to the webpage

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,25 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+   <>
+   <nav className="px-10 py-1 h-12 w-full flex flex-row justify-between items-center border-b-2 border-indigo-500 font-mono shadow-2xl">
+
+    <li className="list-none px-10 font-bold">
+        <ul>LOGO</ul>
+    </li>
+
+    <li className="list-none px-10 font-bold flex flex-row">
+        <ul className="px-5 text-lg cursor-pointer hover:text-sm transition-all duration-200 ease-in-out flex justify-center items-center">HOME</ul>
+        <ul className="px-5 text-lg cursor-pointer hover:text-sm transition-all duration-200 ease-in-out flex justify-center items-center">ABOUT</ul>
+        <ul className="px-5 text-lg cursor-pointer hover:text-sm transition-all duration-200 ease-in-out flex justify-center items-center">CONTACT</ul>
+    </li>
+
+   </nav>
+   </>
+  )
+}
+
+export default Navbar
+
+
